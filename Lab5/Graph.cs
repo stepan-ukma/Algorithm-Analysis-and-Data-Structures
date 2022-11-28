151b44259a880afc50d8e82eb62255c0fb9bf443
stepan-ukma <s.kalika@ukma.edu.ua>
2022-11-28 05:57:42 +0200

    Implement topological sorting algorithm

---
+++ Lab5/Graph.cs
@@ -0,0 +1,57 @@
+﻿namespace Lab5
+{
+    public class Graph
+    {
+        private int verticesCount;
+
+        // Adjacency List
+        private List<List<int>> adjacencyList;
+
+        public Graph(int vertices)
+        {
+            verticesCount = vertices;
+            adjacencyList = new List<List<int>>();
+
+            for (int i = 0; i < vertices; i++)
+                adjacencyList.Add(new List<int>());
+        }
+
+        public void AddEdge(int vertex, int adjVertex) 
+        { 
+            adjacencyList[vertex].Add(adjVertex); 
+        }
+        public void TopologicalSort()
+        {
+            Stack<int> stack = new Stack<int>();
+
+            // Mark all the vertices as not visited
+            var visited = new bool[verticesCount];
+
+            // Visit from each vertix if not already visited
+            for (int i = 0; i < verticesCount; i++)
+            {
+                if (visited[i] == false)
+                    SortUtil(i, visited, stack);
+            }
+
+            foreach (var vertex in stack)
+            {
+                Console.Write(vertex + " ");
+            }
+        }
+
+        private void SortUtil(int start, bool[] visited, Stack<int> stack)
+        {
+            visited[start] = true;
+
+            // Recur for all the vertices adjacent to this vertex
+            foreach (var vertex in adjacencyList[start])
+            {
+                if (!visited[vertex])
+                    SortUtil(vertex, visited, stack);
+            }
+
+            stack.Push(start);
+        }
+    }
+}
---
+++ Lab5/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                int verticesCount = 0;
+
+                try
+                {
+                    Console.Write("Please enter the number of vertices in the graph: ");
+                    verticesCount = int.Parse(Console.ReadLine());
+
+                    // Create a graph
+                    Graph g = new Graph(verticesCount);
+
+                    Console.WriteLine();
+                    Console.WriteLine($"Please enter the pairs of adjacency vertices (enter 'stop' to finish creating the graph): ");
+
+                    int count = 1;
+                    while (true)
+                    {
+                        string[] inputString;
+
+                        while (true)
+                        {
+                            Console.Write($"{count}: ");
+
+                            inputString = Console.ReadLine().Trim().Split();
+
+                            try
+                            {
+                                var u = Convert.ToInt32(inputString[0]);
+                                var v = Convert.ToInt32(inputString[1]);
+
+                                if (u >= verticesCount || v >= verticesCount)
+                                {
+                                    Console.WriteLine($"Invalid input! Vertix should less than the number of vertices in the graph! ");
+                                    Console.WriteLine();
+                                }
+                                else
+                                {
+                                    g.AddEdge(u, v);
+                                    count++;
+                                }
+
+                            }
+                            catch (Exception ex)
+                            {
+                                if (inputString[0] == "stop") break;
+                                else
+                                {
+                                    Console.WriteLine($"Invalid input! " + ex.Message);
+                                    Console.WriteLine();
+                                }
+                            }
+                        }
+
+                        break;
+                    }
+
+                    Console.WriteLine();
+                    Console.WriteLine("Topological sort of the created graph: ");
+                    g.TopologicalSort();
+
+                    break;    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Invalid input! " + ex.Message);
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
